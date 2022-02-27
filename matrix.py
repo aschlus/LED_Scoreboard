364bb0faa2b5e1a181e516368c1e045be4b4641f
Andrew Schlusberg <aschlus@optonline.net>
2022-02-26 20:32:35 -0500

    Created branch, moved matrix.py

--- matrix.py
+++ matrix.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python
 import time
 import sys
+import cairosvg
+
+from io import BytesIO
 
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 
-if len(sys.argv) < 2:
-    sys.exit("Require an image argument")
-else:
-    image_file = sys.argv[1]
+# if len(sys.argv) < 2:
+#     sys.exit("Require an image argument")
+# else:
+#     image_file = sys.argv[1]
+
+url = 'https://assets.nhle.com/logos/nhl/svg/WSH_light.svg'
+
+out = BytesIO()
+cairosvg.svg2png(url=url, write_to=out)
+image_file = Image.open(out)
+image_file = image_file.crop(image_file.getbbox())
 
 image = Image.open(image_file)
 
 # Configuration for the matrix
 options = RGBMatrixOptions()
 options.rows = 32
+options.cols = 64
 options.chain_length = 1
 options.parallel = 1
-options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+options.hardware_mapping = 'adafruit-hat-pwm'
+options.gpio_slowdown = 2
 
-matrix = RGBMatrix(options = options)
+matrix = RGBMatrix(options=options)
 
 # Make image fit our screen.
 image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
